d50320c184a2e1fbe01ff7b6bf407cb750c2d8cb
Jamie Dawson <jadawson@e1z2r4p30.42.us.org>
2019-03-18 11:51:14 -0700

    full project

---
+++ graph.py
@@ -0,0 +1,58 @@
+import turtle
+
+t = turtle.Turtle()
+t.speed(20)
+
+t.down()
+t.goto(0,0) #middle
+t.write ("0", font=("Arial", 0, "normal"))
+
+t.goto(0, 100) #left
+t.write("Y 100", font=("Arial", 12, "normal"))
+t.goto(0,0)
+
+t.goto(-100, 0)#right
+t.write("X -100", font=("Arial", 12, "normal"))
+t.goto(0,0)
+
+t.goto(100,0)#right
+t.write("100", font=("Arial", 12, "normal"))
+t.goto(0,0)
+
+t.goto(0,-100)# bottom
+t.write("-100", font=("Arial", 12, "normal"))
+
+
+t.up()
+t.goto(50, 50)
+t.down()
+t.color("red")
+t.circle(10)
+
+
+t.up()
+t.goto(50, -50)
+t.color("blue")
+t.down()
+t.circle(10)
+
+
+t.up()
+t.goto(-100,-100)
+t.down()
+t.color("Yellow")
+for i in range(3):
+  t.forward(30)
+  t.left(120)
+
+
+
+t.up()
+t.goto(-100,100)
+t.down()
+t.color("green")
+for i in range(4):
+  t.forward(20)
+  t.right(90)
+
+t.ht() #hide turtle
